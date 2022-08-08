2d3ba22f330534f2a4efa004203cf18e88651e16
jjou33 <56063287+jjou33@users.noreply.github.com>
2022-08-08 17:00:15 +0900

    FEAT: object binding 학습내용 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,30 @@
 <template>
+  <div v-memo="[subscriber]">
+    <p>subscribers: {{ subscriber }}</p>
+    <p>views: {{ views }}</p>
+    <p>likes: {{ likes }}</p>
+  </div>
+  <button @click="subscriber++">sub++</button>
+  <button @click="views++">views++</button>
+  <button @click="likes++">likes++</button>
   <div>
-    <h2>{{ teacher.name }}</h2>
-    <h2>강의가 있나여?</h2>
-    <p>computed 미사용 : {{ teacher.lecture.length > 0 ? '있음' : '없음' }}</p>
-    <p>computed 사용 : {{ hasLecture }}</p>
-    <p>computed 사용 : {{ existedLecture() }}</p>
-    <h2>블로그 명</h2>
-    <p>{{ fullName }}</p>
+    <p>subscribers: {{ subscriber }}</p>
+    <p>views: {{ views }}</p>
+    <p>likes: {{ likes }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, computed, ref } from 'vue'
+import { ref } from 'vue'
 
 export default {
   setup() {
-    const teacher = reactive({
-      name: 'hippo',
-      lecture: ['HTML/CSS', 'Javascript', 'Vue3']
-    })
-
-    // computed 의 경우 여러번 호출되었을떄
-    // 한번 계산되었을때 결과를 cache 에 저장해두고
-    // 다음 호출 때 뿌려준다.
-    // 따라서. method 보다 computed 로 사용하는게 효율적이다.
-    const hasLecture = computed(() => {
-      console.log('computed')
-      return teacher.lecture.length > 0 ? '있음' : '없음'
-    })
-
-    // 반면 method 의 경우 cache 가 일어나지 않고 계속 실행된다.
-    const existedLecture = () => {
-      console.log('method')
-      return teacher.lecture.length > 0 ? '있음' : '없음'
-    }
-    // 즉, 브라우저가 재랜더링을 진행하더라도 다시 계산하지 않고 cache 된
-    // 결과를 사용하므로 성능상 유리하다.
-
-    const firstName = ref('Hippo')
-    const lastName = ref('Dev')
-    // const fullName = computed(() => {
-    //   return firstName.value + ' ' + lastName.value
-    // })
-    // 아래와 같이 computed 된 속성을 변경할 경우 readonly 로 인해 불가능하다.
-    // fullName.value = 'Dev Hippo'
-
-    // 따라서, get, set 함수를 통해 변경해야한다.
-    const fullName = computed({
-      get() {
-        return firstName.value + ' ' + lastName.value
-      },
-      set(value) {
-        ;[firstName.value, lastName.value] = value.split(' ')
-      }
-    })
-
-    // get, set 함수를 정의하면 가능하다.
-    fullName.value = 'Dev Hippo'
-
-    return {
-      teacher,
-      hasLecture,
-      existedLecture,
-      fullName
-    }
+    const subscriber = ref(4000)
+    const views = ref(400)
+    const likes = ref(20)
+    // v-memo 의 경우 배열안에 명시한 값이 변경할때만 재랜더링함
+    // 나머지는 v-once 와 비슷
+    return { subscriber, views, likes }
   }
 }
 </script>
---
+++ src/examVue/App ObjectBinding_styleBinding.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="text" :class="{ active: isActive, 'text-danger': hasError }">
+      텍스트 입니다.
+    </div>
+
+    <button @click="toggle">toggle</button>
+    <button @click="checkError">CheckError</button>
+    <div :class="classObject">클레스 오브젝트</div>
+    <div :class="classObjectCompute">클레스 오브젝트 Compute</div>
+
+    <div :style="styleObject">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolore
+      vitae est hic dignissimos, repudiandae voluptate obcaecati quasi odit!
+      Repellendus, tempore autem saepe ea illo iure similique atque. Corporis,
+      ipsa!
+    </div>
+    <button @click="fontSize--">-</button>
+    <button @click="fontSize++">+</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    const isActive = ref(true)
+    const hasError = ref(false)
+    const toggle = () => {
+      isActive.value = !isActive.value
+    }
+    const checkError = () => {
+      hasError.value = !hasError.value
+    }
+    const classObject = reactive({
+      active: true,
+      'text-danger': true
+    })
+
+    const classObjectCompute = computed(() => {
+      return {
+        active: false,
+        'text-danger': true
+      }
+    })
+
+    // styleObject
+    const fontSize = ref(14)
+    const styleObject = computed(() => {
+      return {
+        color: 'red',
+        fontSize: fontSize.value + 'px'
+      }
+    })
+    return {
+      classObjectCompute,
+      isActive,
+      toggle,
+      hasError,
+      checkError,
+      classObject,
+      styleObject,
+      fontSize
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.active {
+  font-weight: 900;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
