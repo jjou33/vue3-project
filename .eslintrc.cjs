af1e9998eba50eeb238389e5e0306bfc9499d936
jjou33 <56063287+jjou33@users.noreply.github.com>
2022-08-08 14:22:47 +0900

    STYLE: 코드 포맷팅 , 린팅 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,31 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'none',
+        printWidth: 80,
+        arrowParens: 'avoid',
+        endOfLine: 'auto'
+      }
+    ]
+  }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -8,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '~': resolve(__dirname, 'src')
     }
   }
 })
